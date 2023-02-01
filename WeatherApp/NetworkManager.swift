f067cb7d3b35a90338cdd59043807b8bb6fde4e3
Ken Yeh <s1011451@gmail.com>
2023-02-01 16:24:41 +0800

    Add NetworkManager.swift File & Add Api Enum and NetworkManager Class with request Function

---
+++ WeatherApp/NetworkManager.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkManager.swift
+//  WeatherApp
+//
+//  Created by Ken on 2023/2/1.
+//
+
+import Foundation
+
+enum Api: String {
+    
+    case currentWeather = "/weather"
+    
+    private var baseUrl: String { "https://api.openweathermap.org/data/2.5" }
+    
+    var path: String { baseUrl + self.rawValue }
+}
+
+class NetworkManager {
+    
+    func request(url: URL, resultHandler: ((Data?) -> Void)? = nil) {
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                resultHandler?(data)
+            }
+            if let response = response as? HTTPURLResponse {
+                print("statusCode:", response.statusCode)
+            }
+            if let error = error {
+                print("error:",error)
+                return
+            }
+        }
+        task.resume()
+    }
+    
+}
