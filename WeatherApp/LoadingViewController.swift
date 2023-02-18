6ccaa62d70457944074e619e8439b2e8e5cfb9f6
Ken Yeh <s1011451@gmail.com>
2023-02-18 10:50:16 +0800

    Add String extension File

--- WeatherApp/LoadingViewController.swift
+++ WeatherApp/LoadingViewController.swift
@@ -146,22 +146,3 @@ extension LoadingViewController {
         return string.containsValidCharacter
     }
 }
-
-//禁止使用者輸入特殊字元
-extension String {
-    var containsValidCharacter: Bool {
-        guard self != "" else { return true }
-        let noNeedToRestrict = CharacterSet(charactersIn: " ") //可以打空白
-        if noNeedToRestrict.containsUnicodeScalars(of: self.last!) {
-            return true
-        } else {
-            return CharacterSet.alphanumerics.containsUnicodeScalars(of: self.last!)
-        }
-    }
-}
-
-extension CharacterSet {
-    func containsUnicodeScalars(of character: Character) -> Bool {
-        return character.unicodeScalars.allSatisfy(contains(_:))
-    }
-}
